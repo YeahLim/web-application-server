bd719c2827f5c4efbcfa4ae3a06409da718144b6
SSAFY <SSAFY@DESKTOP-DOGVPUB>
2023-05-03 16:01:24 +0900

    요구사항 4 : 302 status code 적용

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -54,6 +54,8 @@ public class RequestHandler extends Thread {
         		Map<String, String> params = HttpRequestUtils.parseQueryString(body);
         		User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
         		log.debug("User : {}", user);
+        		DataOutputStream dos = new DataOutputStream(out);
+        		response302Header(dos, "/index.html");
         	} else {
         		DataOutputStream dos = new DataOutputStream(out);
         		byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
@@ -65,7 +67,18 @@ public class RequestHandler extends Thread {
         }
     }
 
-    private int getContentLength(String line) {
+    private void response302Header(DataOutputStream dos, String url) {
+		try {
+			dos.writeBytes("HTTP/1.1 302 Redirect \r\n");
+			dos.writeBytes("Location: " + url + "\r\n");
+			dos.writeBytes("\r\n");
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		}
+		
+	}
+
+	private int getContentLength(String line) {
 		String[] headerTokens = line.split(":");
 		return Integer.parseInt(headerTokens[1].trim());
 	}
