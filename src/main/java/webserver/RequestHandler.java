1082321f626722a4b8370fb244ec94f1131de7b0
SSAFY <SSAFY@DESKTOP-DOGVPUB>
2023-05-03 15:31:52 +0900

    요구사항 2 : GET방식으로 회원가입하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -9,9 +9,13 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import model.User;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -40,10 +44,18 @@ public class RequestHandler extends Thread {
         		line = br.readLine();
         	}
         	String url = tokens[1];
-        	byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
-            DataOutputStream dos = new DataOutputStream(out);
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+        	if (url.startsWith("/user/create")) {
+        		int index = url.indexOf("?");
+        		String queryString = url.substring(index + 1);
+        		Map<String, String> params = HttpRequestUtils.parseQueryString(queryString);
+        		User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+        		log.debug("User : {}", user);
+        	} else {
+        		DataOutputStream dos = new DataOutputStream(out);
+        		byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+        		response200Header(dos, body.length);
+        		responseBody(dos, body);
+        	}
         } catch (IOException e) {
             log.error(e.getMessage());
         }
